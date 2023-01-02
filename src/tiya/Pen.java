bb31ce2907c655a540862cf8ecdedfe3ac61c309
noumanjanu <mdnouman86@gmail.com>
2023-01-02 10:36:30 +0530

    hoooo

---
+++ src/tiya/Pen.java
@@ -0,0 +1,10 @@
+package tiya;
+
+public class Pen {
+
+	public static void main(String[] args) throws Exception {
+		FileInputStream file= new FileInputStream(C:\Users\NOUMAN\Documents\\salma);
+
+	}
+
+}
